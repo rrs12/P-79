b334c1e13d8b8e18b7a2049b40951909d81a849c
rrs12 <77575080+rrs12@users.noreply.github.com>
2021-04-19 13:34:46 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+var menu = [
+    "Paneer Tikka Pizza",
+    "Veg Supreme Pizza",
+    "Exotic Pizza",
+    "Chef Special Pizza",
+    "Deluxe Veggie Pizza"
+];
+
+function getmenu() {
+    var htmldata;
+    htmldata = "<ol class='menulist'>"
+        menu.sort();
+    for (var i = 0; i < menu.length; i++) {
+        htmldata = htmldata + "<li>" + menu[i] + "</li>"
+    }
+    htmldata = htmldata + "</ol>"
+    document.getElementById("display_menu").innerHTML = htmldata;
+}
+
+function add_item() {
+    console.log("I am inside add item")
+    var htmldata;
+    var item = document.getElementById("add_item").value;
+    menu.push(item);
+    menu.sort();
+
+    htmldata = "<section class='cards'>"
+    for (var j = 0; j < menu.length; j++) {
+        htmldata = htmldata + "<div class='card'>" + "<img src=images/pizzaImg.png>" + menu[j] + "</div>"
+    }
+
+    htmldata = htmldata + "</section>"
+    document.getElementById("display_addmenu").innerHTML = htmldata;
+}
